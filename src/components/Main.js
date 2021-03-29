8c7182af045cc6841daefb9fc9a62710431a7bce
dongmin <rlaehdals753@naver.com>
2021-03-29 20:36:57 +0900

    Main.js - 아이템 이미지

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,50 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import {
+  faArchive,
+  faShoppingCart,
+  faBookOpen,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Main() {
   return (
     <div>
+      {/* eslint-disable jsx-a11y/anchor-is-valid  */}
       <section className="main">
         <div className="home">
           <h2>홈</h2>
         </div>
         <ul className="home_item">
-          <li className="hom_item1">
-            <a href="#">
-              <img src="" alt="" />
+          <li className="home_item1">
+            <a href="">
+              <div className="item_inner">
+                <FontAwesomeIcon icon={faUser} size="2x" />
+              </div>
               정보수정
             </a>
           </li>
-          <li className="hom_item1">
+          <li className="home_item1">
             <a href="#">
+              <div className="item_inner">
+                <FontAwesomeIcon icon={faArchive} size="2x" />
+              </div>
               <img src="" alt="" /> 상품보기
             </a>
           </li>
-          <li className="hom_item1">
+          <li className="home_item1">
             <a href="#">
+              <div className="item_inner">
+                <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+              </div>
               <img src="" alt="" /> 장바구니
             </a>
           </li>
-          <li className="hom_item1">
+          <li className="home_item1">
             <a href="#">
+              <div className="item_inner">
+                <FontAwesomeIcon icon={faBookOpen} size="2x" />
+              </div>
               <img src="" alt="" /> 게시판
             </a>
           </li>
@@ -36,5 +55,3 @@ function Main() {
 }
 
 export default Main;
-
-/* eslint-disable jsx-a11y/anchor-is-valid  */
