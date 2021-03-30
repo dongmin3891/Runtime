07902be403f97a6b20d7714ebe48d4fe365c0db2
dongmin <rlaehdals753@naver.com>
2021-03-30 23:55:57 +0900

    css reset 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
+import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import styled from "styled-components";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Header />
       <Main />
     </div>
