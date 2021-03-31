b45ab6460863980c104cedcfb3717f4f894638d6
dongmin <rlaehdals753@naver.com>
2021-03-31 23:44:56 +0900

    Header.js 가로 정렬 중..

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,52 @@
 import React from "react";
+import styled from "styled-components";
+
+const Navbar = styled.div`
+  background-color: black;
+  padding-top: 48px;
+  padding-left: 48px;
+  h1 {
+    color: red;
+    font-size: 34px;
+  }
+
+  a {
+    color: white;
+  }
+
+  ul {
+  }
+  li {
+    display: inline-block;
+    margin-right: 15px;
+    font-size: 20px;
+  }
+`;
 
 function Header() {
   return (
-    <div className="header">
-      {/* eslint-disable jsx-a11y/anchor-is-valid  */}
-      <a href="#">
-        <h1 className="header_title">Shop</h1>
-      </a>
-      <ul>
-        <li>
-          <a href="">내 정보</a>
-        </li>
-        <li>
-          <a href="">상품</a>
-        </li>
-        <li>
-          <a href="">장바구니</a>
-        </li>
-        <li>
-          <a href="">게시판</a>
-        </li>
-      </ul>
-    </div>
+    <Navbar>
+      <div className="header">
+        {/* eslint-disable jsx-a11y/anchor-is-valid  */}
+        <a href="#">
+          <h1 className="header_title">Dong Dong Shop</h1>
+        </a>
+        <ul>
+          <li>
+            <a href="">내 정보</a>
+          </li>
+          <li>
+            <a href="">상품</a>
+          </li>
+          <li>
+            <a href="">장바구니</a>
+          </li>
+          <li>
+            <a href="">게시판</a>
+          </li>
+        </ul>
+      </div>
+    </Navbar>
   );
 }
 
