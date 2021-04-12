bbcb56ee5aa2371b43329a859b77fe0d96de9934
dongmin <rlaehdals753@naver.com>
2021-04-12 17:46:08 +0900

    Item.js main title add

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const ItemSection = styled.section`
+  margin: 120px 120px;
+
+  /* main title */
+  h2 {
+    font-size: 24px;
+    font-weight: bold;
+    border-left: 3px solid #ff4e00;
+    margin: 20px 0 50px 20px;
+    padding-left: 15px;
+  }
+`;
 
 function Item() {
   return (
-    <div>
-      <h1 color="red">Hello world</h1>
-    </div>
+    <ItemSection>
+      <section>
+        <h2>상품</h2>
+      </section>
+    </ItemSection>
   );
 }
 
